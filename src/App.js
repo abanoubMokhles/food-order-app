0e7190bccf56a0dee0bd8a900ff515154aa5a90c
abanoub mokhles <mokhles.abanoub1@gmail.com>
2023-02-26 17:30:48 +0200

    starting over

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
-import React, { useState } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartContextProvider from "./context/CartContextProvider";
 function App() {
-  const [cartIsVisible, setCartIsVisible] = useState(false);
-
-  const showCartHandler = () => {
-    setCartIsVisible(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsVisible(false);
-  };
-
-  return (
-    <CartContextProvider>
-      {cartIsVisible && <Cart onClose={hideCartHandler} />}
-      <Header onClick={showCartHandler} />
-      <main role="main">
-        <Meals />
-      </main>
-    </CartContextProvider>
-  );
+  return <h1>starting over</h1>;
 }
 
 export default App;
