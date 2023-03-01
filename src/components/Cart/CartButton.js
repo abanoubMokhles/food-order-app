06025138337c39340d2a68bddcb62994a1298a66
abanoub mokhles <mokhles.abanoub1@gmail.com>
2023-03-01 17:30:29 +0200

    add bump feature to the cart button

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,19 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import CartIcon from "../UI/CartIcon";
 import CartContext from "../../context/cart-context";
 import styles from "./CartButton.module.scss";
 
 const CartButton = (props) => {
   const cartContx = useContext(CartContext);
-  const totalItems = cartContx.items.reduce((acc, curItem) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
+
+  const { items } = cartContx;
+  const totalItems = items.reduce((acc, curItem) => {
     return acc + curItem.amount;
   }, 0);
   const clickHandler = () => {
     props.onshowCart();
   };
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(true);
+    }, 300);
+
+    return () => {
+      setButtonIsHighlighted(false);
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  const cartBtnClasses = `${styles.button} ${
+    buttonIsHighlighted ? styles.bump : ""
+  }`;
+
   return (
-    <button type="button" className={styles.button} onClick={clickHandler}>
+    <button type="button" className={cartBtnClasses} onClick={clickHandler}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
