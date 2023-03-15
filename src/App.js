972f5d9c062de2557721316f890a70ef00e1a714
abanoub mokhles <mokhles.abanoub1@gmail.com>
2023-03-15 17:51:44 +0200

    reset app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React, { useState } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartContextProvider from "./context/CartContextProvider";
+
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
+  
   return (
-    <CartContextProvider>
-      {cartIsShown && <Cart onCloseCart={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main role="main">
-        <Meals />
-      </main>
-    </CartContextProvider>
+    <p>Hi</p>
   );
 }
 
